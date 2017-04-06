40ef3f801bf1629522a2bf18a896d25cafdf2429
MadDreamland <maddreamland@gmail.com>
2017-04-06 19:30:14 +0800

    完善功能

--- BaiduNuoMi/WebCrawlingAnalysisService_1/task.js
+++ BaiduNuoMi/WebCrawlingAnalysisService_1/task.js
@@ -1,28 +1,60 @@
 /**
  * Created by 10399 on 2017/4/6.
  */
-var word = '前端';
+
+phantom.outputEncoding = 'gb2312';
+
 var page = require('webpage').create();
-// page.includeJs('library/jquery-3.2.0.min.js', )
-var time = Date.now();
+var system = require('system');
+var word = encodeURI(system.args[1]);
+var t = Date.now();
+var result = {
+    code: 1, //返回状态码，1为成功，0为失败
+    msg: '抓取成功', //返回的信息
+    word: system.args[1], //抓取的关键字
+    time: '2000ms', //任务的时间
+    dataList:[   //抓取结果列表
+        {
+            title: 'xx',  //结果条目的标题
+            info: '', //摘要
+            link: '', //链接
+            pic: '' //缩略图地址
+        }
+    ]
+};
+
 page.open('https://www.baidu.com/s?wd=' + word, function (status) {
-    console.log(status);
-    var code = (status === 'success') ? 1 : 0;
-    var content = page.evaluate(function () {
-        return document.querySelector('#content_left');
-    });console.log(content);
-    var list = content.querySelectorAll('[class~=result]');
-    var dataList = [];
-    for (var i = 0, item; item = list[i++];) {
-        dataList.push({
-            title: item.querySelector('a').innerText,
-            info: item.querySelector('.c-abstract').innerText,
-            link: item.querySelector('a').href,
-            pic: item.querySelector('.c-img').src
+    if (status === 'success') {
+        result.code = 1;
+        result.msg = '抓取成功';
+        result.dataList = page.evaluate(function () {
+            var dataList = [];
+            var $search = document.querySelector('#content_left');
+            var $list = $search.querySelectorAll('.result, .result-op');
+            for (var i = 0, item; item = $list[i++];) {
+                var $a = item.querySelector('a');
+                var $pic = item.querySelector('.c-img');
+                var $info = item.querySelector('.c-abstract') ||    //普通摘要
+                    (item.querySelector('.c-span-last p')) ||   //百度百科摘要
+                    item.querySelector('.op_dict_text2') ||   //百度词典摘要
+                    item.querySelector('.op-tieba-general-subtitle');       //百度贴吧某个吧的摘要
+                dataList.push({
+                    title: $a && $a.innerText,
+                    info: $info && $info.innerText,
+                    link: $a && $a.href,
+                    pic: $pic && $pic.src
+                });
+            }
+            return dataList;
         });
+        result.time = (Date.now() - t) + 'ms';
     }
-    console.log(dataList[0].title);
-    var time = (Date.now() - time) + 'sec';
-    console.log(time);
+    else {
+        result.code = 0;
+        result.msg = '抓取失败';
+        result.time = (Date.now() - t) + 'ms';
+    }
+    console.log(JSON.stringify(result, undefined, 4));
+    return result;
     phantom.exit();
 });
